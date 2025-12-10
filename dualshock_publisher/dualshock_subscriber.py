5b4b9b91b5e62bcd87de732e249a02b4b45395d1
jwgcurrie <joelcurrie3@gmail.com>
2025-12-10 21:59:11 +0000

    hostname and port are now configured with .env file. Defaults to http://localhost:5556/

--- dualshock_publisher/dualshock_subscriber.py
+++ dualshock_publisher/dualshock_subscriber.py
@@ -1,16 +1,19 @@
 import zmq
 import json
+import os
 
 context = zmq.Context()
 subscriber = context.socket(zmq.SUB)
 
 # Connect to the publisher running in the Docker container
-subscriber.connect("tcp://localhost:5556")
+zmq_host = os.environ.get('ZMQ_HOST', 'localhost')
+zmq_port = os.environ.get('ZMQ_PORT', '5556')
+subscriber.connect(f"tcp://{zmq_host}:{zmq_port}")
 
 # Subscribe to all messages (empty subscription)
 subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
 
-print("ZeroMQ subscriber connected to tcp://localhost:5556")
+print(f"ZeroMQ subscriber connected to tcp://{zmq_host}:{zmq_port}")
 print("Waiting for Dualshock controller input...")
 
 try:
